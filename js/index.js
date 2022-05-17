960ed5770ddd14a568a450ca56a0ad971cdc619d
charlypomarico <carla.pomarico@evolv.ai>
2022-05-17 20:55:12 -0300

    uploading final project NOT FINISHED YET

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// //
+
+// function responsiveFunction() {
+//   var x = document.getElementById("thenavbar");
+//   if (x.className === "navbar") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navbar";
+//   }
+// }
+
+// // Make Menu Responsive 
+
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", mobileMenu);
+
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// }
+
+// const navLink = document.querySelectorAll(".nav-link");
+
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }
+
+// Add the sticky class 
+
+// function stickyFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
