45a68df4246295f91ed5accb42e12a90c24d5e83
charlypomarico <carla.pomarico@evolv.ai>
2022-05-22 22:13:07 -0300

    adding stuff for switch language

--- js/index.js
+++ js/index.js
@@ -1,48 +1,10 @@
-
-$( document ).ready(function () {
-
-    // By default
-    $('.en_lang').addClass("active-lang");
-    $('#lang-switch .en').addClass("active-side");
-
-    // Function switch
-    $(function () {
-        // Spanish button
-        $("#lang-switch .es").click(function () {
-
-            // Enable Spanish
-            $('.es_lang').addClass("active-lang");
-
-            // Disable English
-            $('.en_lang').removeClass("active-lang");
-
-        });
-
-        // English button
-        $("#lang-switch .en").click(function () {
-
-            // Enable English
-            $('.en_lang').addClass("active-lang");
-
-            // Disable Spanish
-            $('.es_lang').removeClass("active-lang");
-        });
+$(function () {
+    $("#english").click(function () {
+        $(".en-lang").fadeOut();
+        $(".es-lang").fadeIn();
+    });
+    $("#spanish").click(function () {
+        $(".es-lang").hide();
+        $(".en-lang").show();
     });
 });
-
-/* $(function(){
-// Kui ma klikin ID-ga eng elemendi peale
-$("#eng").click(function(){
-  //klassiga german, kao vaikselt ära
-  $(".german").fadeOut();
-     //klassiga english, tule vaikselt esile
-   $(".english").fadeIn();
-});
-// Kui ma klikin ID-ga de elemendi peale
-$("#de").click(function(){
-    //klassiga english, kao vaikselt ära
-   $(".english").hide();
-      //klassiga german, tule vaikselt esile
-  $(".german").show();
-});
-}); */
